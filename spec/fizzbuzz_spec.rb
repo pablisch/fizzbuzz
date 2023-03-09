b73aafe27360fb2e3d94f3294eafeaa5d70fb83b
Pablo <pablito@me.com>
2023-03-09 13:37:57 +0000

    wrote tests for some multiples of 3 and 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,8 +5,25 @@ describe "fizzbuzz" do
     expect(fizzbuzz(3)).to(eq('fizz'))
   end
 
+  it "returns 'fizz' when passed 18" do
+    expect(fizzbuzz(18)).to(eq('fizz'))
+  end
+
+  it "returns 'fizz' when passed 333" do
+    expect(fizzbuzz(333)).to(eq('fizz'))
+  end
+
   it "returns 'buzz' when passed 5" do
     expect(fizzbuzz(5)).to(eq('buzz'))
   end
-end
 
+  it "returns 'buzz' when passed 20" do
+    expect(fizzbuzz(20)).to(eq('buzz'))
+  end
+
+  it "returns 'buzz' when passed 555" do
+    expect(fizzbuzz(555)).to(eq('buzz'))
+  end
+
+  
+end
